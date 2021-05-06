ebf81f61205b40f9aea974e36175e2d9562c10b4
Hugo-flahaut <hugoflahautfeucher@gmail.com>
2021-05-06 20:53:36 +0200

    Update with initialised project

---
+++ front-app/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-3">
+          <Navbar />
+        </div>
+        <div class="col-9">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
